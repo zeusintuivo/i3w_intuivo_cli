89c7fc9bd56a18c6cdbcaf4ac342a78bcbaaa216
zeusintuivo <zeus@intuivo.com>
2024-04-20 09:58:03 +0300

    added copy for rust i3 status

--- install_i3wm_to_config.bash
+++ install_i3wm_to_config.bash
@@ -8,7 +8,6 @@ function instalar() {
 		autorandr
 		dunst
 		flameshot
-		i3
 		i3-dot-files
 		i3-keyboard-layout
 		i3status
@@ -36,32 +35,61 @@ function instalar() {
     ln -s "${cwd}/${one}"  "${HOME}/.config/${one}"
 	} done <<< "${lista}"
   local -i _err=0
+
+	[[ -L "${HOME}/.config/install_i3wm_to_config.bash" ]] && 	unlink "${HOME}/.config/install_i3wm_to_config.bash"
+	[[ -e "${HOME}/.config/install_i3wm_to_config.bash" ]] && 	rm -rf "${HOME}/.config/install_i3wm_to_config.bash"
+
+
+	[[ -L "${HOME}/.config/i3" ]] && 	unlink "${HOME}/.config/i3"
+	[[ -e "${HOME}/.config/i3" ]] && 	rm -rf "${HOME}/.config/i3"
+  [[ ! -d "${HOME}/.config/i3" ]] &&  mkdir -p "${HOME}/.config/i3"
   if [[ -e "${HOME}/.config/i3/config" ]]; then
 	{
 		cp "${HOME}/.config/i3/config" "${HOME}/.config/i3/config.bk.${CURRENTDATE}${CURRENTTIME}"
     _err=$?
 		if [ ${_err} -gt 0 ]; then
 		{
-      echo "error saving copy err=$_err"
+      echo "$0:$LINENO error saving copy \"${HOME}/.config/i3/config.bk.${CURRENTDATE}${CURRENTTIME}\" err=$_err"
 			return 1
 		}
 		fi
 	}
 	fi
 
+	echo "$0:$LINENO  mkdir -p \"${HOME}/.local/share/\" "
+	[[ -L "${HOME}/.local/share/" ]] && 	unlink "${HOME}/.local/share/"
+	[[ -e "${HOME}/.local/share/" ]] && 	rm -rf "${HOME}/.local/share/"
+  #v[[ ! -d "${HOME}/.local/share/" ]] &&  mkdir -p "${HOME}/.local/share/"
+  [[ ! -L "${HOME}/.local/share/i3status-rust" ]] &&   ln -s "${cwd}/i3status-rust" "${HOME}/.local/share/i3status-rust"
+
+  # if [[ -e "${HOME}/.local/share/i3status-rust/config.toml" ]]; then
+	# {
+  #   # cp -R "${cwd}/i3status-rust" "${HOME}/.local/share/"
+	# 	cp "${HOME}/.local/share/i3status-rust/config.toml" "${HOME}/.local/share/i3status-rust/config.toml.bk.${CURRENTDATE}${CURRENTTIME}"
+  #   _err=$?
+	# 	if [ ${_err} -gt 0 ]; then
+	# 	{
+  #     echo "$0:$LINENO error saving copy \"${HOME}/.local/share/i3status-rust/config.toml.bk.${CURRENTDATE}${CURRENTTIME}\"  err=$_err"
+	# 		return 1
+	# 	}
+	# 	fi
+	# }
+	# fi
+
+
   local _msg=""
-  echo "cp \"${cwd}/i3/config\"  \"${HOME}/.config/i3/config\""
+  echo "$0:$LINENO cp \"${cwd}/i3/config\"  \"${HOME}/.config/i3/config\""
 	_msg=$(cp "${cwd}/i3/config"  "${HOME}/.config/i3/config" 2>&1 )
 	_err=$?
   if [ ${_err} -gt 0 ]; then
   {
 		if [[ "${_msg}" == *"are the same file"*  ]] ; then
 		{
-			echo ""
+			echo "$0:$LINENO ok "
 		}
 		else
 		{
-      echo "error making ${cwd}/i3/config  ${HOME}/.config/i3/config  err=$_err"
+      echo "$0:$LINENO error making ${cwd}/i3/config  ${HOME}/.config/i3/config  err=$_err"
       return 1
 		}
 		fi
@@ -73,7 +101,7 @@ function instalar() {
   _err=$?
  	if [ ${_err} -gt 0 ]; then
   {
-    echo "error changing ${HOME}/.config/i3/config text to /home/zeus to $HOME err=$_err"
+    echo "$0:$LINENO error changing ${HOME}/.config/i3/config text to /home/zeus to $HOME err=$_err"
     return 1
   }
   fi
